3fb945de806833e4fce9fcbfae0c39705a6c0331
mpalkowski <maxence.palkowski1@gmail.com>
2024-07-14 10:18:36 +0200

    Changed aboutMe

--- portfolio/src/components/bodies/aboutMe/AboutMe.tsx
+++ portfolio/src/components/bodies/aboutMe/AboutMe.tsx
@@ -7,7 +7,9 @@ export default function AboutMe(){
     <div className="Body About">
         <img src={pp} alt="" />
         <h1>{data.content.title}</h1>
-        <p>{data.content.body}</p>
+        <p className='AboutP1'>{data.content.bodyFr1}</p>
+        <p className='AboutP2'>{data.content.bodyFr2}</p>
+        <p className='AboutP3'>{data.content.bodyFr3}</p>
 
     </div>
     )
--- portfolio/src/components/bodies/home/Home.tsx
+++ portfolio/src/components/bodies/home/Home.tsx
@@ -4,8 +4,8 @@ import './home.scss';
 export default function Home(){
     return(
         <div className="Body Home">
-            <h1>{data.content.title}</h1>
-            <p>{data.content.body}</p>
+            <h1>{data.content.titleFr}</h1>
+            <p>{data.content.bodyFr}</p>
         </div>
     )
 }
