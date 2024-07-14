51e9d659de88b8003cabc0a5680333f3acf41d85
mpalkowski <maxence.palkowski1@gmail.com>
2024-07-14 11:52:30 +0200

    Structure Work

--- portfolio/src/components/bodies/education/Education.tsx
+++ portfolio/src/components/bodies/education/Education.tsx
@@ -4,22 +4,22 @@ import "./education.scss";
 export default function Education() {
   return (
     <div className="Body Education">
-    <div className="Rotary">
-      <h1>{data.content.french.rotary.title}</h1>
-      <p>{data.content.french.rotary.paragraph.phrase1}</p>
-      <p>{data.content.french.rotary.paragraph.phrase2}</p>
-      <p>{data.content.french.rotary.paragraph.phrase3}</p>
+      <div className="Rotary">
+        <h1>{data.content.french.rotary.title}</h1>
+        <p>{data.content.french.rotary.paragraph.phrase1}</p>
+        <p>{data.content.french.rotary.paragraph.phrase2}</p>
+        <p>{data.content.french.rotary.paragraph.phrase3}</p>
       </div>
-    <div className="UPHF">
-      <h1>{data.content.french.uphf.title}</h1>
-      <p>{data.content.french.uphf.paragraph.phrase1}</p>
-      <p>{data.content.french.uphf.paragraph.phrase2}</p>
-      <p>{data.content.french.uphf.paragraph.phrase3}</p>
+      <div className="UPHF">
+        <h1>{data.content.french.uphf.title}</h1>
+        <p>{data.content.french.uphf.paragraph.phrase1}</p>
+        <p>{data.content.french.uphf.paragraph.phrase2}</p>
+        <p>{data.content.french.uphf.paragraph.phrase3}</p>
       </div>
-    <div className="ISEN">
-      <h1>{data.content.french.isen.title}</h1>
-      <p>{data.content.french.isen.paragraph.phrase1}</p>
-      <p>{data.content.french.isen.paragraph.phrase2}</p>
+      <div className="ISEN">
+        <h1>{data.content.french.isen.title}</h1>
+        <p>{data.content.french.isen.paragraph.phrase1}</p>
+        <p>{data.content.french.isen.paragraph.phrase2}</p>
       </div>
     </div>
   );
--- portfolio/src/components/bodies/work/Work.tsx
+++ portfolio/src/components/bodies/work/Work.tsx
@@ -2,13 +2,31 @@ import "./work.scss";
 import data from "../../../assets/json/work.json";
 
 export default function Work() {
+  const ubiMission = data.content.ubisoft.missions.map(({ id, mission }) => (
+    <li key={id} id={id} className={`ubiMission${id}`}>
+      {mission}
+    </li>
+  ));
+
+  const strMission = data.content.straton.missions.map(({ id, mission }) => (
+    <li key={id} id={id} className={`ubiMission${id}`}>
+      {mission}
+    </li>
+  ));
   return (
     <div className="Body Work">
       <div className="Ubisoft">
-        <h1 className="Title">{data.content.ubisoft.title}</h1>
-        <p className="Description">{data.content.ubisoft.description}</p>
+        <h1 className="UTitle">{data.content.ubisoft.title}</h1>
+        <p className="UDescription">{data.content.ubisoft.description}</p>
         <h2 className="MissionTitle">{data.content.ubisoft.tilte2}</h2>
-        <p className="Missions">{data.content.ubisoft.missions}</p>
+        <p className="UMissions">{ubiMission}</p>
+      </div>
+      <span className="Delimiter"></span>
+      <div className="Straton">
+        <h1 className="STitle">{data.content.straton.title}</h1>
+        <p className="SDescription">{data.content.straton.description}</p>
+        <h2 className="MissionTitle">{data.content.straton.tilte2}</h2>
+        <p className="SMissions">{strMission}</p>
       </div>
     </div>
   );
